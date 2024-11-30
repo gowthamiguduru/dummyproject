b2a1c5957c65ff4729cb818b02bcdcfef3a3de25
Gowthami <guduru.gowthu@gmail.com>
2024-11-30 19:43:46 +0530

    added additional checks

--- src/Loanpage.java
+++ src/Loanpage.java
@@ -5,6 +5,15 @@ public class Loanpage {
 		// TODO Auto-generated method stub
 String s="loan approved";
 System.out.println(s);
+String s1="loan Approved";
+if(s.equalsIgnoreCase(s1))
+{
+	System.out.println("success");
+}
+else
+{
+	System.out.println("not success");
+}
 	}
 
 }
